9f53c7c6def632462abedb479454687e6893ea6d
Tanguy Jouvin <tanguyj28@yahoo.fr>
2019-03-07 14:50:00 +0100

    HUGE conflict map

--- text.js
+++ text.js
@@ -1,4 +1,4 @@
-	//Here is the functionality of the slider//
+	//Here is the functionality of the slider
 
 	wow = new WOW(
 		{
@@ -16,25 +16,26 @@
 		this.parentNode.insertBefore(section, this);
 	  };
 	
-	//Here is the fantastik part of code for the functionality of the map//
+//Here is the map of the site
 
-			// On initialise la latitude et la longitude de Paris (centre de la carte)
-			var lat = 48.852969;
-			var lon = 2.349903;
-			var macarte = null;
-			// Fonction d'initialisation de la carte
-			function initMap() {
-				// Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-                macarte = L.map('map').setView([lat, lon], 11);
-                // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
-                L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-                    // Il est toujours bien de laisser le lien vers la source des données
-                    attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-                    minZoom: 1,
-                    maxZoom: 20
-                }).addTo(macarte);
-            }
-			window.onload = function(){
-				// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
-				initMap(); 
-			};
+	// On initialise la latitude et la longitude de Nantes (centre de la carte)
+	var lat = 47.218371;
+	var lon =  -1.553621;
+	var macarte = null;
+	// Fonction d'initialisation de la carte
+	function initMap() {
+		// Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+					var	macarte = L.map('map').setView([lat, lon], 11);
+						// Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+						L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+								// Il est toujours bien de laisser le lien vers la source des données
+								attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+								minZoom: 1,
+								maxZoom: 20
+						}).addTo(macarte);
+				}
+	window.onload = function(){
+		// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+		initMap(); 
+	};
+</script>
